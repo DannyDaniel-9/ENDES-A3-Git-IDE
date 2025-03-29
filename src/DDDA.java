145efe90c238aab178ca9f19028c6677ec2560e8
DannyDaniel-9 <3dldadaniel26@gmail.com>
2025-03-29 22:06:20 +0100

    Modificacion en la clase DDDA

--- src/DDDA.java
+++ src/DDDA.java
@@ -2,6 +2,8 @@
 public class DDDA {
         public static void main(String [] args) {
                  System.out.println("Creacion de clase de prueba para conexion con GITHUB desde ECLIPSE");
+                 
+                 System.out.println("Modificacion para prueba de segundo commit");
         }
 
 }
